417eca75c79ef3c4705c722d9c1ca59cc3ce9ddb
Hakan ÖRCÜN <31225072+orcnhakan@users.noreply.github.com>
2019-07-01 20:43:45 +0300

    Add files via upload

---
+++ EksiSozluk.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+import random
+import time
+
+browser = webdriver.Firefox()
+
+url = "https://eksisozluk.com/mustafa-kemal-ataturk--34712?p="
+"""
+p page durumunu durumudur,bir sonraki gelen rakam entriylerin sayfa numarasıdır
+
+
+"""
+
+
+pageCount = 1
+entries = []
+entryCount = 1
+
+while pageCount <= 10:
+    randomPage = random.randint(1,1290)
+    newUrl = url + str(randomPage)
+    browser.get(newUrl)
+
+    elements = browser.find_elements_by_css_selector(".content")
+    for element in elements:
+        entries.append(element.text)
+    time.sleep(3)
+    pageCount += 1
+
+with open("entries.txt","w",encoding = "UTF-8") as file:
+    for entry in entries:
+        file.write(str(entryCount) + ".\n" + entry + "\n")
+        file.write("***********************************\n")
+        entryCount += 1
+
+
+
+browser.close()
+
+
+
